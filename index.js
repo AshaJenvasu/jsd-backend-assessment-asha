b47aafab8c1f62927099ae856e2b0f78b4d440ad
AshaJenvasu <chaiyawatjenvasu@gmail.com>
2026-05-26 10:02:03 +0700

    Set up In-memory Array and Request log middle ware

--- index.js
+++ index.js
@@ -3,8 +3,37 @@ import express from "express";
 const app = express();
 const PORT = 3000;
 
+let products = [
+  {
+    id: 1,
+    name: "Fated One of Miracles, Rezael",
+    price: 300,
+    quantity: 1,
+  },
+  {
+    id: 2,
+    name: "Divine Sister, Biscotti",
+    price: 2500,
+    quantity: 2,
+  },
+  {
+    id: 3,
+    name: "Knight of Gentle Beauty, Nobia",
+    price: 100,
+    quantity: 1,
+  },
+];
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  const now = new Date().toISOString();
+  console.log(`${now}: ${req.method} ${req.url}`);
+  next();
+});
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Server is running smoothly!");
 });
 
 app.listen(PORT, () => {
